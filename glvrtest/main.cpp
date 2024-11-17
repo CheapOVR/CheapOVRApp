41ed203dbcc6036af2204c9df2ae254fed0388ac
nasOS-official <petruhin-enot@yandex.ru>
2024-11-17 20:23:43 +0400

    Добавление поддержки отключения ble при сборке

--- glvrtest/main.cpp
+++ glvrtest/main.cpp
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #ifdef __arm__
 #include <GLES2/gl2.h>
 // #include <GLES2/gl2ext.h>
@@ -15,14 +16,17 @@
 #include <iostream>
 #include <libserialport.h>
 #include <ostream>
-#include <simpleble/SimpleBLE.h>
 #include <thread>
 #include "cmdline.h"
+
+#ifdef BLE
+#include <simpleble/SimpleBLE.h>
+
 
 #define SERVICE_UUID           "6e400001-b5a3-f393-e0a9-e50e24dcca9e" 
 #define CHARACTERISTIC_UUID_RX "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 #define CHARACTERISTIC_UUID_TX "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
-
+#endif
 std::atomic<bool> needDataFlag{};
 std::atomic<bool> running{};
 uint8_t read_buf[8];
@@ -60,23 +64,28 @@ void readingThread(char *port_name) {
     check(sp_set_baudrate(port, 115200));
     check(sp_set_bits(port, 8));
     check(sp_set_parity(port, SP_PARITY_NONE));
-    check(sp_set_stopbits(port, 1));
+    check(sp_set_stopbits(port, 2));
     check(sp_set_flowcontrol(port, SP_FLOWCONTROL_NONE));
+    // check(sp_set_flowcontrol(port, SP_FLOWCONTROL_RTSCTS));
     check(sp_nonblocking_write(port, "S", 1));
     while (running.load()) {
       if (needDataFlag.load()) {
 
         uint8_t read_buf_tmp[8];
-        int num_bytes = check(sp_nonblocking_read(port, read_buf_tmp, 8));
+        int num_bytes = check(sp_blocking_read(port, read_buf_tmp, 8, 2));
         if (num_bytes < 0 ||
             !(read_buf_tmp[0] == 0xFE && read_buf_tmp[7] == 0xFF)) {
-
+         // if (read_buf[0] == 0xFE && read_buf[7] == 0xFF) {
         } else if (read_buf_tmp[0] == 0xFE && read_buf_tmp[7] == 0xFF) {
-          memset(&read_buf, '\0', sizeof(read_buf));
+          memset(&read_buf, 0, sizeof(read_buf));
           for (int x = 0; x < 8; x++) {
             read_buf[x] = read_buf_tmp[x];
           }
         }
+        for (int x = 0; x < 8; x++) {
+            printf("%d ", read_buf_tmp[x]);
+        }
+        printf("\n");
 
         needDataFlag.store(false);
       }
@@ -86,7 +95,9 @@ void readingThread(char *port_name) {
     sp_free_port(port);
 
 
-  } else if (wrk_mode == 1) {
+  } 
+  #ifdef BLE
+  else if (wrk_mode == 1) {
     if (!SimpleBLE::Adapter::bluetooth_enabled()) {
       std::cout << "Bluetooth is not enabled" << std::endl;
       abort();
@@ -135,6 +146,11 @@ void readingThread(char *port_name) {
     peripheral.disconnect();
     std::cout << "Disconnected!" << std::endl;
   }
+  #endif
+  else { 
+    std::cout << "Unsupported mode. Try to compile an application with support for this mode." << std::endl;
+    exit(2);
+  }
 }
 
 // void window_size_callback(GLFWwindow* window, int width, int height){
@@ -174,8 +190,6 @@ void renderingThread() {
     needDataFlag.store(true);
   while (needDataFlag.load()) {
   }
-
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_MODELVIEW);
@@ -260,31 +274,15 @@ void renderingThread() {
 }
 
 int main(int argc, char *argv[]) {
-  if (argv)
-  if (argc == 1) {
-    std::cout << "Required one or more arguments" << std::endl;
-    return 1;
-  }
+  gengetopt_args_info args_info;
+  if (cmdline_parser (argc, argv, &args_info) != 0)
+    exit(1);
+   if (args_info.ble_flag) wrk_mode = 1;
 
-  if (argc >= 3) {
-    int num = std::atoi(argv[2]);
-    if (num == 1) {
-      inbox = true;
-    } else {
-      inbox = false;
-    }
-  }
-  if (argc >= 4) {
-    int num = std::atoi(argv[3]);
-    if (num <= 3)
-      wrk_mode = num;
-    else
-      abort();
-  }
   running.store(true);
 
   std::thread render(renderingThread);
-  std::thread thread(readingThread, argv[1]);
+  std::thread thread(readingThread, args_info.device_arg);
   render.join();
   // thread.join();
   thread.detach();
